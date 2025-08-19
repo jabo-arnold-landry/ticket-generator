eb6e47e06bf8b87d171b7006547fbc54d720c961
jabo arnold landry <arnoldjab@gmail.com>
2025-08-19 19:22:46 +0200

    added-functionality-to-handle-files-both-drag-and-input

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,34 @@
-console.log("ticket generator");
+const dragEvents = ["dragenter", "dragover", "dragleave", "drop"];
+const draggableElement = document.querySelector(".drag");
+const fileInputElement = document.querySelector("[type=file]");
+fileInputElement.addEventListener("change", function (e) {
+  displayImage(fileInputElement.files[0]);
+});
+dragEvents.forEach((event) =>
+  draggableElement.addEventListener(event, function (e) {
+    e.preventDefault();
+  })
+);
+dragEvents.slice(0, 2).forEach((event) => {
+  draggableElement.addEventListener(event, activeState);
+});
+dragEvents.slice(2).forEach((event) => {
+  draggableElement.addEventListener(event, notActiveState);
+});
+
+draggableElement.addEventListener("drop", function (e) {
+  const dataTransfer = e.dataTransfer;
+  const image = dataTransfer.files;
+  displayImage(image[0]);
+});
+function activeState() {
+  draggableElement.classList.add("dragging");
+}
+function notActiveState() {
+  draggableElement.classList.remove("dragging");
+}
+function displayImage(img) {
+  const imgUrl = URL.createObjectURL(img);
+  const avatarElement = draggableElement.querySelector("img");
+  avatarElement.src = imgUrl;
+}
